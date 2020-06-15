c68a747c96012c00a36ea65e3dd6ef1077a38f58
Long Nguyen <Long.Nguyen@temple.edu>
2020-06-15 13:02:26 -0400

    Updating gh-pages

---
+++ build/precache-manifest.efa146a3d37ef17705d7f86953ee5b7c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91d67cd9df0a692d095ef97f0525133",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "e027f5650021bb28bc5a",
+    "url": "/React-Portfolio/static/css/2.929bb6f5.chunk.css"
+  },
+  {
+    "revision": "58258c8cd8a61ab5b009",
+    "url": "/React-Portfolio/static/css/main.5a7e7495.chunk.css"
+  },
+  {
+    "revision": "e027f5650021bb28bc5a",
+    "url": "/React-Portfolio/static/js/2.7e360b59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.7e360b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58258c8cd8a61ab5b009",
+    "url": "/React-Portfolio/static/js/main.f296e666.chunk.js"
+  },
+  {
+    "revision": "c4913e1c011bdf21278d",
+    "url": "/React-Portfolio/static/js/runtime-main.9aa55fa6.js"
+  },
+  {
+    "revision": "2b8c25c796d03b0252fc2fcc6e03074f",
+    "url": "/React-Portfolio/static/media/README.2b8c25c7.gif"
+  },
+  {
+    "revision": "573e903f615e2914ea6fc118fea6013f",
+    "url": "/React-Portfolio/static/media/Resume.573e903f.pdf"
+  },
+  {
+    "revision": "43039741c53a1c37431f423f843f8969",
+    "url": "/React-Portfolio/static/media/employee.43039741.gif"
+  },
+  {
+    "revision": "1a89c218821464448a83fc65725c93cd",
+    "url": "/React-Portfolio/static/media/fyreapp.1a89c218.png"
+  },
+  {
+    "revision": "9f775ddea258db3ed011b75dd6a57943",
+    "url": "/React-Portfolio/static/media/password.9f775dde.png"
+  },
+  {
+    "revision": "a3628bfb82c87880b01575607df309f8",
+    "url": "/React-Portfolio/static/media/shark.a3628bfb.png"
+  }
+]);
